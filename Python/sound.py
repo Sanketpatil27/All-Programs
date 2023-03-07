f5e50f7e33ba6940d664c4afb15a114951d0490e
Sanket Patil <90706710+Sanketpatil27@users.noreply.github.com>
2023-03-07 18:23:11 +0530

    Create sound.py

---
+++ Python/sound.py
@@ -0,0 +1,19 @@
+# this is an comment
+''' 
+this is multiline comment
+'''
+# print("hello world")
+
+# for multiline print -->
+# print('''hello worsjr
+# sdjf kl
+# sdfgfgld''')
+
+import os
+from playsound import playsound
+
+# install an external module and use it to perform an operation
+playsound('Water.mp3')
+
+# use of os module 
+print(os.listdir())
